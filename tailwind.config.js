301f2c22ff987f9837833cc29c3ece4f5eb58c68
Aqsamajeed00 <aqsamajeedking313@gmail.com>
2024-10-03 22:49:01 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./src/app/**/*.{js,ts,jsx,tsx}", // Path for files under the 'app' directory
+      "./src/app/components/**/*.{js,ts,jsx,tsx}", // Path for component files
+      "./pages/**/*.{js,ts,jsx,tsx}" // Path for page files
+    ],
+
   theme: {
     extend: {},
   },
