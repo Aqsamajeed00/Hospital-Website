7e43943345196008c0bab42489f293adb20740e1
Aqsamajeed00 <aqsamajeedking313@gmail.com>
2024-11-10 19:11:11 +0500

    push code

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -29,7 +29,7 @@ export default function Contact() {
               <textarea
                 placeholder="Your Message"
                 className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                rows="4"
+                rows={4}
               ></textarea>
             </div>
             <button
